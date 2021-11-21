8d9d5d052b35d09683f740ebf90fddfcfda6f6c3
varsharanitekale <varsharanitekale50@gmail.com>
2021-11-21 21:50:15 +0530

    initial

---
+++ index.js
@@ -0,0 +1,12 @@
+for(m=1; m<=5; m++){
+    for(n=1; n<=m; n++){
+        document.write("*");
+        if(n==m)
+        continue;
+        else 
+        document.write('');
+
+
+    }
+    document.write('<br />');
+}
